769c9f5387ce53125c2c94f065fc7f9c6ff0ac92
Shivam Sharma <shivam14052004@gmail.com>
2026-06-26 11:37:07 +0530

    added vehicle and notofication route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import loggingMiddleware from './logging-middleware/middleware.js';
-
+import vehicleRouter from './vehicle-scheduler-be/vehicle.js';
+import notificationRouter from './notification-app-be/notification.js';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,8 @@ app.get('/', (req, res) => {
     res.json({ message: "Campus Evaluation Root API active" });
 });
 
+app.use('/vehicles', vehicleRouter);
+app.use('/notifications', notificationRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
